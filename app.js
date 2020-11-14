ea297a876fa433516aadab15909b63befe8e89b0
Noal Brown <noalbrown1@gmail.com>
2020-11-14 15:04:43 -0600

    finish

---
+++ app.js
@@ -0,0 +1,83 @@
+const PageState = function () {
+  let currentState = new homeState(this);
+
+  this.init = function () {
+    this.change(new homeState)
+  }
+
+  this.change = function (state) {
+    currentState = state
+  }
+}
+
+// home state
+const homeState = function (page) {
+  document.querySelector('#heading').textContent = null
+  document.querySelector('#content').innerHTML = `
+    <div class="jumbotron">
+      <h1 class="display-3">Hello, World!</h1>
+      <p class="lead">Calls extra attention to features.</p>
+      <hr class="my-4">
+      <p>Utility classes used for spacing</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      </p>
+    </div>
+  `;
+}
+
+// About state
+const aboutState = function (page) {
+  document.querySelector('#heading').textContent = 'About Us'
+  document.querySelector('#content').innerHTML = `
+    <p>This is the about page</p>
+  `;
+}
+
+// Contact state
+const contactState = function (page) {
+  document.querySelector('#heading').textContent = 'Contact Us'
+  document.querySelector('#content').innerHTML = `
+    <form>
+      <div class="form-group">
+        <label>Name</label>
+        <input type="text" class="form-control">
+      </div>
+      <div class="form-group">
+        <label>Email address</label>
+        <input type="email" class="form-control">
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  `;
+}
+
+// Instantiate page state
+const page = new PageState()
+
+// Init the first state
+page.init()
+
+// UI variables
+const home = document.getElementById('home'),
+  about = document.getElementById('about'),
+  contact = document.getElementById('contact');
+
+// Home
+home.addEventListener('click', (e) => {
+  page.change(new homeState)
+
+  e.preventDefault()
+})
+// about
+about.addEventListener('click', (e) => {
+  page.change(new aboutState)
+
+  e.preventDefault()
+})
+// contact
+contact.addEventListener('click', (e) => {
+  page.change(new contactState)
+
+  e.preventDefault()
+})
